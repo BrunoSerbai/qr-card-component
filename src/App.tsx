05df2cb94c7046f6215564bbe2a9b248e6f2dc74
Bruno Serbai <brunoserbai@outlook.com>
2025-02-10 00:39:25 -0300

    fix image error

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="qrCard bg-white p-5 rounded-3xl max-w-[21rem] font-[outfit] text-center m-3 ">
-      <img className="rounded-xl" src="public/image-qr-code.png"/>
+      <img className="rounded-xl" src="/image-qr-code.png"/>
       <h3 className="font-bold text-[22px] m-4 mt-7 ">Improve your front-end skills by building projects</h3>
       <p className="text-gray-600 text-[15px] m-5   ">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
     </div>
